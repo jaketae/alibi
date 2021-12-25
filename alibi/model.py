7e718d46402002904b8385c1b53f822e38ac18b1
Jake Tae <jaesungtae@gmail.com>
2021-12-25 13:31:56 +0900

    fix: add assert for `max_len` check

--- alibi/model.py
+++ alibi/model.py
@@ -8,9 +8,12 @@ from alibi.layers import ALiBiTransformerLayer
 class ALiBiTransformer(nn.Module):
     def __init__(self, config: ALiBiConfig) -> None:
         super().__init__()
+        self.max_len = config.max_len
         self.layers = nn.Sequential(
             *[ALiBiTransformerLayer(config) for _ in range(config.num_layers)]
         )
 
     def forward(self, x: torch.tensor) -> torch.tensor:
+        _, seq_len, _ = x.shape
+        assert seq_len <= self.max_len, "sequence length exceeds `max_len`"
         return self.layers(x)
